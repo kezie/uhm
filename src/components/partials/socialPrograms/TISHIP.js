c117e0fc74a298199f6a46998ce72782b4d17402
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-09-17 17:51:47 +0100

    Update TISHIP.js

--- src/components/partials/socialPrograms/TISHIP.js
+++ src/components/partials/socialPrograms/TISHIP.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import otumba from '../../../images/hero-bg-2.jpg'
+import { Link } from 'react-router-dom'
 
 const TISHIP = () => {
   return (
@@ -7,14 +8,14 @@ const TISHIP = () => {
         className="banner-one bg_cover p-r z-1"
         style={{ backgroundImage: `url(${otumba})` }}
     > 
-        <div className="container">
+        <div className="container pt-120">
             <div className="row">
                 <div className="col-xl-7 col-lg-10">
                 {/*=== Hero Content ===*/}
                     <div className="text-white mt-5 mb-5">
-                        <h1 className="wow fadeInUp mb-2" data-wow-delay=".7s" style={{color:'#db812f'}}>
+                        <h2 className="wow fadeInUp mb-4" data-wow-delay=".7s" style={{color:'#db812f'}}>
                             The TISHIP PROGRAMME
-                        </h1>
+                        </h2>
                         <p className="wow fadeInUp mb-2 text-light">
                             The TISHIP programme is one of the National Health Insurance Authority Packages that provides quality Healthcare for the students of tertiary institutions.
                             The Tertiary Institutions Social Health Insurance Programme TISHIP is a system whereby the health care of tertiary institutions is paid for from the funds pooled through students’ contributions.
@@ -22,6 +23,14 @@ const TISHIP = () => {
                         <p className="wow fadeInUp mb-2 text-light">
                             It is a programme committed to ensuring qualitative healthcare services for students of the institutions thereby promoting the health and wellbeing of students.
                         </p>
+                    </div>
+                    <div
+                      className="hero-button wow fadeInDown"
+                      data-wow-delay=".9s"
+                    >
+                      <Link to="#" className={`main-btn btn-success`}>
+                        <span className=""> More Info </span>
+                      </Link>
                     </div>
                 </div>
             </div>
