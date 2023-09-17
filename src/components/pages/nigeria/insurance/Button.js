535449cb1690a2b8fe73017e1564be2c6123e6ed
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-09-17 17:51:20 +0100

    Create Button.js

---
+++ src/components/pages/nigeria/insurance/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Button = () => {
+  return (
+    <Link to="/insurance">
+        <a className="main-btn btn-black">Buy Plan</a>
+    </Link>
+  )
+}
+
+export default Button
