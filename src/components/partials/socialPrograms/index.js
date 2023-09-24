725a0f746e485487eaacfb169722e5b14476e991
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-09-24 20:50:07 +0100

    Update index.js

--- src/components/partials/socialPrograms/index.js
+++ src/components/partials/socialPrograms/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import TISHIP from './TISHIP'
 import GIFSHIP from './GIFSHIP'
+import Brands from '../brands'
 
 const SocialPrograms = () => {
   return (
     <>
         <TISHIP/>
         <GIFSHIP/>
+        <Brands/>
     </>
   )
 }
