7acffcd63bd402084c0d4afbfb5b3cac643e9b42
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-09-25 12:22:59 +0100

    Update index.js

--- src/components/partials/socialPrograms/index.js
+++ src/components/partials/socialPrograms/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import TISHIP from './TISHIP'
 import GIFSHIP from './GIFSHIP'
 import Brands from '../brands'
+import StateHealth from './StateHealth'
 
 const SocialPrograms = () => {
   return (
     <>
-        <TISHIP/>
         <GIFSHIP/>
-        <Brands/>
+        <TISHIP/>
+        <StateHealth/>
     </>
   )
 }
