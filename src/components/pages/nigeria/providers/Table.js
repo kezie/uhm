6552180ef2ceafa0f4bc293ab21008707fd9eaab
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-09-28 08:28:35 +0100

    Update Table.js

--- src/components/pages/nigeria/providers/Table.js
+++ src/components/pages/nigeria/providers/Table.js
@@ -1,61 +1,47 @@
-import React from 'react';
-import { useTable, useFilters } from 'react-table';
+import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
+import { createRoot } from 'react-dom/client';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
 
-const Table = ({ data }) => {
-  const columns = [
-    {
-      Header: 'ID',
-      accessor: 'id'
-    },
-    {
-      Header: 'Name',
-      accessor: 'Health_Care_Provider'
-    },
-    // Add more columns here
-  ];
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import PageBanner from '../../../partials/PageBanner';
+import {uhms_providers} from './Data'
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    setFilter
-  } = useTable(
-    {
-      columns,
-      data
-    },
-    useFilters
-  );
+const Test = () => {
+
+  const [rowData, setRowData] = useState([])
+
+  const columnDefs = [
+    {field: 'id', width:'100px'},
+    {field: 'Health_Care_Provider', filter:true,  floatingFilter: true, width:'400px'},
+    {field: 'Address',  width:'450px',filter:true,  floatingFilter: true},
+    {field: 'State', width:'150px',filter:true,  floatingFilter: true},
+    {field: 'Category',width:'150px',filter:true,  floatingFilter: true}
+  ]
+
+  const defaultColDef = useMemo(() => {
+    return {
+      resizable: true,
+    };
+  }, []);
+
+  useEffect(()=>{
+    setRowData(uhms_providers.dataroot.uhms_providers)
+  }, [])
+
 
   return (
-    <div>
-      <table {...getTableProps()} className="table">
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+      <div className='ag-theme-alpine' style={{height:600,width:'100%' }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          pagination={true}
+          paginationPageSize={10}
+          defaultColDef={defaultColDef}
+        />
+      </div>
   );
 };
 
-export default Table;
+export default Test;
