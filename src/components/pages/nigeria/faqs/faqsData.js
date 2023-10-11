4d2268687fd8177eaf02d0ceae363da3b839d28b
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-10-11 22:00:21 +0100

    Create faqsData.js

---
+++ src/components/pages/nigeria/faqs/faqsData.js
@@ -0,0 +1,37 @@
+export const faqsData = [
+    {
+        id:1,
+        question:'Which hospitals are on your network?',
+        answer:'Ultimate Health HMO works with more than 10,000 NHIS-accredited facilities. Our network of hospitals spans all the states of Nigeria and the FCT. Our network allows our enrollees enjoy access to all primary and specialist care in whatever part of the country they reside.',
+    },
+    {
+        id:2,
+        question:'Is there a waiting period after registering?',
+        answer:'If you sign up for our private health plans, you can access care as soon as you are registered with no waiting period. If you are signed up for any of the government-subsidized health insurance plans, your waiting period can range from 30-90 days depending on the issuing authority.',
+    },
+    {
+        id:3,
+        question:'How do I choose a hospital?',
+        answer:'We generally advise that enrollees pick hospitals that are closest to them. On our private plans, you will have access to a band of hospitals based on the plan you purchase. You can change your choice of hospital by letting us know. For social health insurance programs, the regulating authority handles the change of your primary facility.',
+    },
+    {
+        id:4,
+        question:'How do I buy a plan?',
+        answer:'Click the button “Get Health Insurance” in the top menu. Fill out the form. If you are not yet sure what insurance plan to buy, our representative would advise you based off the details you filled. Billing and Policy details will be done electronically after we have reviewed your information with you.',
+    },
+    {
+        id:5,
+        question:"What are Ultimate Health HMO’s Contact details?",
+        answer:"For service enquiries call - (234)1 8890002 or (234)1 8870017 For Sales enquiries call – 0803 289 3000 or 0811 966 8483. You can also send an email to info@ultimatehealthhmo.com",
+    },
+    {
+        id:6,
+        question:'Can I reach Ultimate Health HMO on Holidays and weekends?',
+        answer:'Yes. We are available all 24 hours, of every day of the year. As a healthcare service provider, we make your well-being our primary business.',
+    },
+    {
+        id:7,
+        question:'Can I continue with my old hospital on my insurance plan?',
+        answer:'Yes, you can. As long as your hospital is one of the many NHIS-accredited hospitals on our network.',
+    }
+]
