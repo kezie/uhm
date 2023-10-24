b56305b40e5913e65a5eb2df70c853570668f404
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-10-24 15:11:33 +0100

    Update index.js

--- src/components/pages/nigeria/programmes/index.js
+++ src/components/pages/nigeria/programmes/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PageBanner from '../../../partials/PageBanner'
 import SocialPrograms from '../../../partials/socialPrograms'
+import Framer from '../../../partials/Framer'
 
 const Programs = () => {
   return (
     <>
-        <PageBanner pageName={"Social Health Insurance"} PageImage={"wcu2.png"} />
-        <SocialPrograms/>
+      <Framer/>
+      <PageBanner pageName={"Social Health Insurance"} PageImage={"wcu2.png"} />
+      <SocialPrograms/>
     </>
   )
 }
