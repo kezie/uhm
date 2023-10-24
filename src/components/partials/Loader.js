60c89cb90fb8020c6f2a342d05cf2b5d9a3e0771
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-10-24 13:14:38 +0100

    Update Loader.js

--- src/components/partials/Loader.js
+++ src/components/partials/Loader.js
@@ -1,77 +1,14 @@
-import React from "react";
-import { motion } from "framer-motion";
-
-const loadingContainer = {
-  width: "4rem",
-  height: "4rem",
-  display: "flex",
-  justifyContent: "space-around",
-};
-const loadingCircle = {
-  display: "block",
-  width: "1rem",
-  height: "1rem",
-  backgroundColor: "#8eb850",
-  borderRadius: "0.5rem",
-};
-
-const loadingContainerVariants = {
-  start: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-  end: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const loadingCircleVariants = {
-  start: {
-    y: "0%",
-  },
-  end: {
-    y: "60%",
-  },
-};
-const loadingCircleTransition = {
-  duration : 0.4,
-  yoyo : Infinity,
-  ease: 'easeInOut'
-}
+import React from 'react'
+import { Spinner } from 'react-bootstrap'
 
 const Loader = () => {
   return (
-    <div>
-      <div className="fixed  w-full min-h-screen z-50 bg-black opacity-30" />
-      <div className="flex fixed w-full justify-center items-center h-screen">
-        <motion.div
-          style={loadingContainer}
-          variants={loadingContainerVariants}
-          initial="start"
-          animate="end"
-        >
-          <motion.span
-            style={loadingCircle}
-            variants={loadingCircleVariants}
-            transition={loadingCircleTransition}
-          ></motion.span>
-          <motion.span
-            style={loadingCircle}
-            variants={loadingCircleVariants}
-            transition={loadingCircleTransition}
-          ></motion.span>
-          <motion.span
-            style={loadingCircle}
-            variants={loadingCircleVariants}
-            transition={loadingCircleTransition}
-          ></motion.span>
-        </motion.div>
-      </div>
+    <div className='my-5 text-center'>
+      <Spinner animation='grow' role='status'>
+        <span className='visually-hidden'></span>
+      </Spinner>
     </div>
-  );
-};
+  )
+}
 
-export default Loader;
+export default Loader
