7afb9c2e7bd2ebd75169fc23485b2fed87d8e3d7
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-01 07:34:47 +0100

    Create data.js

---
+++ src/components/pages/global/data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+        id:'1',
+        icon:'far fa-hospital',
+        link:'/providers',
+        title:'Our Providers',
+        text:'We provide healthcare for our enrollees in all states of Nigeria. We are partnered with all NHIA-accredited healthcare providers in the country to provide unrivalled healthcare for you and your loved ones.'
+    },
+    {
+        id:'2',
+        icon:'fas fa-file-contract',
+        link:'/insurance-plans',
+        title:'See Our Plans',
+        text:'Our bouquet of plans offer a comprehensive range of benefits that help individuals and families stay financially protected and secure. Invest in Peace of mind for you and your loved ones.'
+    },
+    {
+        id:'3',
+        icon: 'fab fa-researchgate',
+        link:'',
+        title:'Our Research',
+        text:'Read more about healthcare administration in Nigeria. In our periodical publications we keep you informed about all the advancements in healthcare in Nigeria.'
+    },
+    {
+        id:'4',
+        icon: 'fas fa-info-circle',
+        link:'',
+        title:'Get Information',
+        text:'Our Diaspora plan is continually adding features and benefits to improve the quality of care your loved ones receive. Join our mailing list to stay up to date on the new features and benefits available.'
+    }
+
+]
