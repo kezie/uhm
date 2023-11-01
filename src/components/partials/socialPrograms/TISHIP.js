fbc36a44c57eb12826325c2a45eb7f97b09ce60f
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-01 09:00:09 +0100

    Update TISHIP.js

--- src/components/partials/socialPrograms/TISHIP.js
+++ src/components/partials/socialPrograms/TISHIP.js
@@ -29,8 +29,8 @@ const TISHIP = () => {
                       className="hero-button wow fadeInDown"
                       data-wow-delay=".9s"
                     >
-                      <Link to="#" className={`main-btn btn-success`}>
-                        <span className=""> More Info </span>
+                      <Link to="#" className={`main-btn btn-success`} data-bs-toggle="modal" data-bs-target="#formModal">
+                        <span className=""> Learn More </span>
                       </Link>
                     </div>
                 </div>
