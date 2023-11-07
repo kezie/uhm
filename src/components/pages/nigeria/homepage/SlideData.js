386a09c63ed25960d26961c3ff98e36e67d4509d
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-07 10:02:41 +0100

    Update SlideData.js

--- src/components/pages/nigeria/homepage/SlideData.js
+++ src/components/pages/nigeria/homepage/SlideData.js
@@ -1,6 +1,4 @@
-import Video from '../../../../videos/hero.mp4'
 import Hero1 from '../../../../videos/hero2.mp4'
-
 
 export const SlideData = [
     {
@@ -8,13 +6,7 @@ export const SlideData = [
       content: Hero1,
       caption: 'Video Caption 1',
       colorOverlay: 'rgba(0, 0, 0, 0.5)'
-    },
-    // {
-    //   type: 'image',
-    //   content: Hero1,
-    //   caption: 'Image Caption 1',
-    //   colorOverlay: ''
-    // }
+    }
   ];
 
 export const settings = {
