3fe32aaf79e23c91daa5b3b8fd73fabc1c02bb3d
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-16 09:02:23 +0100

    Create places.js

---
+++ src/components/pages/nigeria/providers/places.js
@@ -0,0 +1,43 @@
+import usePlacesAutocomplete from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+
+import "@reach/combobox/styles.css";
+
+const Places = () => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+  } = usePlacesAutocomplete();
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = (val) => {
+    setValue(val, false);
+  };
+
+  return (
+    <Combobox onSelect={handleSelect} aria-labelledby="demo" className="pt-4">
+      <ComboboxInput value={value} onChange={handleInput} placeholder="Search..." />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
+
+export default Places
