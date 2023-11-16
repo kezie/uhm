19cafc0e5fa1eadf91a98b03b9e3f8d947d020ff
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-16 12:53:22 +0100

    Update Providers.js

--- src/components/pages/nigeria/providers/Providers.js
+++ src/components/pages/nigeria/providers/Providers.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import PageBanner from '../../../partials/PageBanner';
-import Table from './Table';
 import Framer from '../../../partials/Framer';
 import Map from '../providers/Map'
 
