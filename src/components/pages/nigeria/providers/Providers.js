64266eb2f814be703a2c2c8c1327d8f656578590
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-16 09:02:26 +0100

    Update Providers.js

--- src/components/pages/nigeria/providers/Providers.js
+++ src/components/pages/nigeria/providers/Providers.js
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import PageBanner from '../../../partials/PageBanner';
 import Table from './Table';
 import Framer from '../../../partials/Framer';
+import Map from '../providers/Map'
 
 const Providers = () => {
 
@@ -9,16 +10,8 @@ const Providers = () => {
       <>
         <Framer/>
         <PageBanner pageName={"Provider Network"} PageImage={'providers.jpg'} />{" "}
-        
-      <section>
-        <div>
-          <h3 className='title text-center my-4'>
-            Browse Through Our Health Providers
-          </h3>
-        </div>
-        <Table/>
-      </section>
-    </>
+        <Map/>
+      </>
     );
 }
 
