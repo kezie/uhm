849f640dc9915cc637a9bb1cdc7125e95cfcc545
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-26 16:57:52 +0100

    Update Map.js

--- src/components/pages/nigeria/providers/Map.js
+++ src/components/pages/nigeria/providers/Map.js
@@ -1,34 +1,25 @@
-import React, { useMemo, useCallback, useEffect, useRef, useState } from "react";
-import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
-import { setKey, fromAddress } from "react-geocode";
+import React, { useEffect, useRef, useState } from "react";
 import { uhms_providers } from "./Data";
 import icon from '../../../../images/marker.png'
 import { state } from "./states";
 import { Link } from "react-router-dom";
-import tippy from "tippy.js";
 import 'tippy.js/dist/tippy.css'
-import { Button } from "react-bootstrap";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
-import {Icon} from 'leaflet'
 import L from 'leaflet';
-import MarkerClusterGroup from "react-leaflet-markercluster";
 
 
 const Map = () => {
   const providers = uhms_providers.dataroot.uhms_providers;
-
-  const [selected, setSelected] = useState();
-  const [markers, setMarkers] = useState([]);
   const [filteredProvider, setFilteredProvider] = useState(providers);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedState, setSelectedState] = useState("");
 
-  const mapRef = useRef();
+  const mapRef = useRef(null);
 
-  const center = useMemo(() => ({ lat: 9.0765, lng: 7.3986 }), []);
-  const options = useMemo(() => ({}), []);
-
+  const panToMarker = (lat, lng)=>{
+    mapRef.current.flyTo([lat,lng], 12)
+  }
 
   const filterByCategory = (filteredData) => {
     // Avoid filter for empty string
@@ -87,66 +78,18 @@ const Map = () => {
   }, 
   [selectedCategory, selectedState]);
 
-  useEffect(() => {
-    if (!mapRef.current) {
-      const map = L.map('map', {
-        center: [9.0765, 7.3986],
-        zoom: 7,
-        scrollWheelZoom: false, // Disable scroll wheel zoom
-      });
-  
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-      }).addTo(map);
-  
-      mapRef.current = map;
-    }
-  
-    const geocodeAddress = async (address, callback) => {
-      try {
-        const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${address}`);
-        const data = await response.json();
-  
-        if (data && data.length > 0) {
-          const { lat, lon } = data[0];
-          callback([lat, lon]);
-        }
-      } catch (error) {
-        console.error('Error fetching geocoding data', error);
-      }
-    };
-  
-    const customIcon = new L.Icon({
-      iconUrl: icon,
-      iconSize: [32, 32], // size of the icon
-      iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
-      popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
-    });
-    
-  
-    const uhmsProviders = providers
-  
-    uhmsProviders.forEach(provider => {
-      geocodeAddress(provider.Address, (latlng) => {
-        const marker = L.marker(latlng, { icon: customIcon }).addTo(mapRef.current);
-        marker.bindPopup(`<b>${provider.Health_Care_Provider}</b><br>${provider.Address}`).openPopup();
-      });
-    });
-  }, []);
+  const customIcon = new L.Icon({
+    iconUrl: icon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
 
   return (
     <div className="">
       <div className="row">
         <div className="col-lg-3 bg-dark" style={{height:'100vh', overflow:'scroll'}}>
           <div className="container pt-3">
-            {selected ? (
-              <div className="bg-light p-4" style={{position:"fixed", zIndex:1000, borderRadius:20, border:'2px solid #8cbd53'}}>
-                <h1 className="" style={{fontSize:14}}><i className="fa fa-hospital"></i> PROVIDER: {selected.name}</h1>
-                <h5 style={{fontSize:14}}> <i className="fa fa-map pt-2"></i> ADDRESS: {selected.h_address}</h5>
-                <p> Category: {selected.category}</p> 
-                <p> State: {selected.state}</p>
-              </div>) : ''
-            }
             <div className="pt-4">
               <input className="form-control" placeholder="Search hospital..." onChange={filterByHospital}/>
             </div>
@@ -167,9 +110,8 @@ const Map = () => {
             </div>
 
             <div className="pt-2 text-light">Filter by State : </div>
-            <div id="year-options" onClick={handleStateChange}>
+            <div onClick={handleStateChange}>
               <select
-                  id="brand-input"
                   value={selectedState}
                   onChange={handleStateChange}
                   className="form-control mb-4"
@@ -186,13 +128,7 @@ const Map = () => {
             {
               filteredProvider.map((provider)=>(
                 
-                <Link onClick={()=>
-                  setSelected({
-                    name:provider.Health_Care_Provider, 
-                    h_address: provider.Address, 
-                    category:provider.Category,
-                    state:provider.State
-                  })
+                <Link onClick={() => panToMarker(provider.lat, provider.lng)
                 }>{provider.Health_Care_Provider}</Link>
                 
               ))
@@ -200,7 +136,38 @@ const Map = () => {
           </div>
         </div>
         <div className="col-lg-9">
-          <div id="map" style={{ height: '100vh' }}></div>
+        <MapContainer
+          center={[9.0765, 7.3986]}
+          zoom={7}
+          style={{ width: '100%', height: '100vh' }}
+          scrollWheelZoom = {false}
+          ref={mapRef}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {providers.map((provider) => (
+            <Marker
+              key={provider.id}
+              position={[provider.lat, provider.lng]}
+              icon={customIcon}
+              eventHandlers={{
+                click: () => panToMarker(provider.lat, provider.lng),
+              }}
+            >
+              <Popup>
+                <h1 style={{fontSize:14}}>
+                  {provider.Health_Care_Provider}
+                </h1>
+                <p>
+                  {provider.State} State, Category: {provider.Category}
+                </p>
+                <Link to= {`https://www.google.com/maps/dir/?api=1&destination=${provider.lat},${provider.lng}`} target="_blank">Get Direction</Link>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
         </div>
       </div>
     </div>
