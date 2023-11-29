441c5b1b9ae376c9237acb779e75a030ee8b81e0
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2023-11-29 12:24:01 +0100

    Update ActionButtons.js

--- src/components/pages/checkout/ActionButtons.js
+++ src/components/pages/checkout/ActionButtons.js
@@ -17,11 +17,11 @@ const ActionButtons = (props) => {
       <div>
         <div className="row">
           {props.currentStep > 1 && (
-            <div className="col-lg-6 pt-3" style={{display:'flex', justifyContent:'flex-end', alignItems:'center'}}>
+            <div className="col-6 pt-3 mb-5" style={{display:'flex', justifyContent:'flex-end'}}>
               <button style={{padding:'2px 15px'}} className='main-btn btn-sm btn-outline' onClick={handleBack}>Back</button>
             </div>
           )}
-          <div className="col-lg-6 pt-3">
+          <div className="col-6 pt-3 mb-5">
             {props.currentStep < props.totalSteps && (
               <button style={{padding:'2px 15px'}} className='main-btn btn-sm btn-outline' onClick={handleNext}>Next</button>
             )}
