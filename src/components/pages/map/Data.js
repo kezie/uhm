2b63849914b8eb9c37ae3c9cdaf028d791ec1ccd
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-01-17 12:02:50 +0100

    Create Data.js

---
+++ src/components/pages/map/Data.js
@@ -0,0 +1,43 @@
+export const uhms_providers = {
+    "dataroot": {
+        "@xmlns:od": "urn:schemas-microsoft-com:officedata",
+        "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "@xsi:noNamespaceSchemaLocation": "uhms_providers.xsd",
+        "@generated": "2023-10-27T16:33:43",
+        "Uhms_providers": [
+            {
+                "id": "3",
+                "Health_Care_Provider": "JANET MEMORIAL HOSPITAL",
+                "Address": "245 Aba-Owerri Road, Aba",
+                "State": "Abia",
+                "lat": "5.350910",
+                "lng":"7.212040",
+                "Category": "Classic"
+            }, {
+                "id": "4",
+                "Health_Care_Provider": "ABIA STATE UNIVERSITY TEACHING HOSPITAL",
+                "Address": "ABA",
+                "State": "Abia",
+                "lat": "7.657390",
+                "lng":"5.231690",
+                "Category": "Classic"
+            },{
+                "id": "316",
+                "Health_Care_Provider": "NURSING HOME, MAIDUGURI",
+                "Address": "behind nhis zonal office, off shehu",
+                "State": "Borno",
+                "lat":"11.251360",
+                "lng":"7.461260",
+                "Category": "Classic"
+            }, {
+                "id": "317",
+                "Health_Care_Provider": "KANEM HOSPITAL & MATERNITY",
+                "Address": "152, Tafawa Balewa Road, beside Lamisula Police Station.",
+                "State": "Borno",
+                "lat":"10.208340",
+                "lng":"9.724030",
+                "Category": "Classic"
+            },
+        ]
+    }
+}
