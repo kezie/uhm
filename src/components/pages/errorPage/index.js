ded863c2d6870fadee35ea548399352f9c6497f3
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-01-19 09:44:46 +0100

    Update index.js

--- src/components/pages/errorPage/index.js
+++ src/components/pages/errorPage/index.js
@@ -6,22 +6,22 @@ const Error = () => {
     <section className="error-404-section p-r z-1">
       <div className="shape shape-one animate-float-x">
         <span className="animate-float-x">
-          <img src="assets/images/shape/shape-1.png" alt="" />
+          <img src="/assets/images/shape/shape-1.png" alt="" />
         </span>
       </div>
       <div className="shape shape-two animate-float-y">
         <span>
-          <img src="assets/images/shape/shape-2.png" alt="" />
+          <img src="/assets/images/shape/shape-2.png" alt="" />
         </span>
       </div>
       <div className="shape shape-three animate-float-y">
         <span>
-          <img src="assets/images/shape/shape-3.png" alt="" />
+          <img src="/assets/images/shape/shape-3.png" alt="" />
         </span>
       </div>
       <div className="shape shape-four animate-float-x">
         <span>
-          <img src="assets/images/shape/shape-4.png" alt="" />
+          <img src="/assets/images/shape/shape-4.png" alt="" />
         </span>
       </div>
       <div className="container-fluid">
@@ -30,7 +30,7 @@ const Error = () => {
             <div className="error-404-wrapper text-center pt-100 pb-100">
               <div className="error-content">
                 <div className="error-img mb-30">
-                  <img src="assets/images/gallery/error.png" alt="404 image" />
+                  {/* <img src="/assets/images/gallery/error.png" alt="404 image" /> */}
                 </div>
                 <h2>OPPS! This Page Was Not Found</h2>
                 <p>
