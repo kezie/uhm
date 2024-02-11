dfb3dea675fb3a3be80d249271d0d29110df1468
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-02-11 11:05:11 +0100

    Update Remita.js

--- src/components/pages/checkout/Remita.js
+++ src/components/pages/checkout/Remita.js
@@ -2,10 +2,21 @@ import React, { useState } from 'react'
 import RemitaPayment from "react-remita";
 import Framer from '../../partials/Framer';
 import { useLocation } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Remita = () => {
     const location = useLocation();
+
     const { firstName, lastName, email, amount } = location.state;
+
+    const successAlert = (firstName, transactionId, amount, paymentReference) => {
+      Swal.fire(`Thank you for your payment ${firstName}`, `Amount Paid: NGN${amount} <br/> Transaction ID: ${transactionId} <br/>  Payment Reference: ${paymentReference}`, 'success');
+    };
+
+    const failAlert = (transactionId) => {
+      Swal.fire('Payment Failed Please try again', `Transaction ID: ${transactionId}` ,'danger');
+    };
+
     const [paymentData, setpaymentData] = useState({
         key: process.env.REACT_APP_REMITA_KEY,
         customerId: process.env.REACT_APP_REMITA_CUSTOMER_ID,
@@ -15,14 +26,30 @@ const Remita = () => {
         amount: amount,
         narration: "Insurance Payment",
       });
+
+      const savePaymentInfo = (transactionId, paymentReference) => {
+        const paymentInfo = {
+          firstName: firstName,
+          amount: amount,
+          transactionId: transactionId,
+          paymentReference: paymentReference
+        };
+        localStorage.setItem('paymentInfo', JSON.stringify(paymentInfo));
+      };
+
       let data = {
         ...paymentData,
         onSuccess: function (response) {
           // function callback when payment is successful
+          const { transactionId, paymentReference } = response;
+          successAlert(firstName, transactionId, amount, paymentReference); 
+          savePaymentInfo(transactionId, paymentReference);
           console.log("callback Successful Response", response);
         },
         onError: function (response) {
           // function callback when payment fails
+          const { transactionId } = response;
+          failAlert(transactionId)
           console.log("callback Error Response", response);
         },
         onClose: function () {
@@ -85,7 +112,7 @@ const Remita = () => {
             />
             <RemitaPayment
               remitaData={data}
-              className='main-btn btn-outline btn-sm mt-4 mb-4' // class to style the button
+              className='main-btn btn-outline btn-sm mt-4 mb-4'
               text='Pay Now' //text to show on button
               // add a 'live' prop to use the live urls/keys
             />
